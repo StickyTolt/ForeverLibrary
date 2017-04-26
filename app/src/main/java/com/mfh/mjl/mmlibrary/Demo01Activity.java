1120d8447b3270d488755e146dbcd56311c02ed7
Cosima <495998692@qq.com>
2017-04-26 16:58:49 +0800

    新建项目

---
+++ app/src/main/java/com/mfh/mjl/mmlibrary/Demo01Activity.java
@@ -0,0 +1,13 @@
+package com.mfh.mjl.mmlibrary;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Demo01Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_demo01);
+    }
+}
